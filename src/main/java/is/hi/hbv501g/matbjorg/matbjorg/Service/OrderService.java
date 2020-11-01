b5a4c26ebc752016f6a0b68a24fec259e508c7d7
hallbjorgembla <hes85@hi.is>
2020-11-01 20:51:27 +0000

    Update OrderService.java

--- src/main/java/is/hi/hbv501g/matbjorg/matbjorg/Service/OrderService.java
+++ src/main/java/is/hi/hbv501g/matbjorg/matbjorg/Service/OrderService.java
@@ -8,38 +8,50 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * Interface fyrir Order
+ * Þjónusta fyrir Order
  */
 public interface OrderService {
     /**
+     * Vistar order
+     *
      * @param order hlutur af taginu Order
      * @return hlutur af taginu Order
      */
     Order save(Order order);
 
     /**
+     * eyðir order
+     *
      * @param order hlutur af taginu Order
      */
     void delete(Order order);
 
     /**
+     * finnur alla Order hluti
+     *
      * @return Listi af Order hlutum
      */
     List<Order> findAll();
 
     /**
+     * finnur Order með ákveðið Id
+     *
      * @param id long tala
      * @return Optional Order hlutur sem hefur id sem Id
      */
     Optional<Order> findById(long id);
 
     /**
+     * Finnur öll Order sem hafa buyer sen Buyer
+     *
      * @param buyer hlutur af taginu Buyer
      * @return Listi af Order hlutum sem hafa buyer sem Buyer
      */
     List<Order> findByBuyer(Buyer buyer);
 
     /**
+     * Finnur Order sem hefur buyer sem Buyer og Active=active
+     *
      * @param buyer  hlutur af taginu Buyer
      * @param active boolean gildi
      * @return Hlutur af taginu Order sem hefur buyer sem Buyer og Active=active
@@ -47,12 +59,16 @@ public interface OrderService {
     Order findByBuyerAndActive(Buyer buyer, boolean active);
 
     /**
+     * Staðfestir order og setur Active=false ef gengur upp
+     *
      * @param order hlutur af taginu Order
      * @return hlutur af taginu Order
      */
     Order confirmOrder(Order order);
 
     /**
+     * Skilar heildarverði OrderItem-a í order
+     *
      * @param order hlutur af taginu Order
      * @return double tala
      */
