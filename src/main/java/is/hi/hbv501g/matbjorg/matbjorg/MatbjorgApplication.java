ef8cde650413607188646c4f92da754524a83c15
Ásmundur Óskar Ásmundsson <asmundur31@gmail.com>
2020-11-23 22:13:43 +0000

    halló

--- src/main/java/is/hi/hbv501g/matbjorg/matbjorg/MatbjorgApplication.java
+++ src/main/java/is/hi/hbv501g/matbjorg/matbjorg/MatbjorgApplication.java
@@ -2,18 +2,15 @@ package is.hi.hbv501g.matbjorg.matbjorg;
 
 import is.hi.hbv501g.matbjorg.matbjorg.Service.Implementations.AdvertisementServiceImplementation;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Component;
 
 import java.io.File;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class MatbjorgApplication {
+public class MatbjorgApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         new File(AdvertisementServiceImplementation.UPLOAD_PICTURE_PATH).mkdir();
