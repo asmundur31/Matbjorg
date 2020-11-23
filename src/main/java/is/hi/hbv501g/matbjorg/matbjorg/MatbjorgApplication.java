839d8885df8735ec5d766a8069049184a60abf39
Ásmundur Óskar Ásmundsson <asmundur31@gmail.com>
2020-11-23 22:01:45 +0000

    reyna að laga

--- src/main/java/is/hi/hbv501g/matbjorg/matbjorg/MatbjorgApplication.java
+++ src/main/java/is/hi/hbv501g/matbjorg/matbjorg/MatbjorgApplication.java
@@ -2,13 +2,16 @@ package is.hi.hbv501g.matbjorg.matbjorg;
 
 import is.hi.hbv501g.matbjorg.matbjorg.Service.Implementations.AdvertisementServiceImplementation;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 
+@Configuration
 @SpringBootApplication
 @EnableJpaRepositories
 public class MatbjorgApplication {
