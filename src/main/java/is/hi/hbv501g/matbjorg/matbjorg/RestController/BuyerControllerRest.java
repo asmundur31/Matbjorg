d102cc9b274a92a19defc64eb166227730e4fd1a
Ásmundur Óskar Ásmundsson <asmundur31@gmail.com>
2021-02-24 16:44:37 +0000

    búa til aðgreiningu frá vefsíðu síðunum

--- src/main/java/is/hi/hbv501g/matbjorg/matbjorg/RestController/BuyerControllerRest.java
+++ src/main/java/is/hi/hbv501g/matbjorg/matbjorg/RestController/BuyerControllerRest.java
@@ -6,6 +6,7 @@ import is.hi.hbv501g.matbjorg.matbjorg.Service.BuyerService;
 import is.hi.hbv501g.matbjorg.matbjorg.Service.OrderService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.Optional;
 
 
 @RestController
+@RequestMapping("/rest")
 public class BuyerControllerRest {
     private BuyerService buyerService;
     private OrderService orderService;
