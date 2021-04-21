a885e43a8ce23db6aa8c23cc78c04df85085525a
audurkatarina <akt6@hi.is>
2021-04-21 09:58:41 +0000

    endapunktur fyrir myndir

--- src/main/java/is/hi/hbv501g/matbjorg/matbjorg/RestController/AdvertisementRestController.java
+++ src/main/java/is/hi/hbv501g/matbjorg/matbjorg/RestController/AdvertisementRestController.java
@@ -5,8 +5,14 @@ import is.hi.hbv501g.matbjorg.matbjorg.Entities.*;
 import is.hi.hbv501g.matbjorg.matbjorg.Service.AdvertisementService;
 import is.hi.hbv501g.matbjorg.matbjorg.Service.LocationService;
 import is.hi.hbv501g.matbjorg.matbjorg.Service.SellerService;
+import org.springframework.core.io.Resource;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.*;
 
@@ -18,6 +24,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -37,7 +44,8 @@ public class AdvertisementRestController {
 
     @GetMapping("")
     List<AdvertisementDTO> all() {
-        List<Advertisement> ads = advertisementService.findAll();
+        advertisementService.updateActive();
+        List<Advertisement> ads = advertisementService.findByActive(true);
         List<AdvertisementDTO> adsDTO = new ArrayList<>();
         for (int i = 0; i < ads.size(); i++) {
             Advertisement ad = ads.get(i);
@@ -67,6 +75,30 @@ public class AdvertisementRestController {
 
         AdvertisementDTO adDTO = new AdvertisementDTO(ad);
         return adDTO;
+    }
+
+    @GetMapping("/image/{pictureName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String pictureName, HttpServletRequest request) {
+        // Load file as Resource
+        Resource resource = advertisementService.getImage(pictureName);
+        System.out.println(resource.getFilename());
+        // Try to determine file's content type
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            System.out.println("Could not determine file type.");
+        }
+
+        // Fallback to the default content type if type could not be determined
+        if(contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 
 }
